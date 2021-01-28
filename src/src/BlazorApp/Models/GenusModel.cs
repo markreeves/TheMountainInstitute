327fb74f8a43b43696e65b5c6fe76f3c89672f7d
mark reeves <1750381+markreeves@users.noreply.github.com>
2021-01-28 16:11:43 +0000

    Inital Commit

---
+++ src/src/BlazorApp/Models/GenusModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorApp.Models
+{
+    public class GenusModel
+    {
+        public int Id { get; set; }
+        public string GenusName { get; set; }
+        public string GenusIntro { get; set; }
+        public string GenusIntroImageUrl { get; set; }
+        public byte[] GenusImage { get; set; }
+    }
+}
---
+++ src/src/BlazorApp/Models/KingdomModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BlazorApp.Models
+{
+    public class PlantsModel
+    {
+        public int Id { get; set; }
+        public string KingdomName { get; set; }
+        public string KingdomIntro { get; set; }
+        public string KingImageUrl { get; set; }
+        public byte[] KingdomImage { get; set; }
+    }
+}
