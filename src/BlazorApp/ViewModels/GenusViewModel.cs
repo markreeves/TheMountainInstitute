cab124a86b3f1ea957b7378c0836f69ba2b5c905
mark reeves <1750381+markreeves@users.noreply.github.com>
2021-01-31 15:58:34 +0000

    impliemented GenusViewModel

--- src/BlazorApp/ViewModels/GenusViewModel.cs
+++ src/BlazorApp/ViewModels/GenusViewModel.cs
@@ -1,14 +1,43 @@
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BlazorApp.Models;
+using DataLibrary;
 
 namespace BlazorApp.ViewModels
 {
     public interface IGenusViewModel
     {
-        
+        List<GenusModel> Genus { get; set; }
+        List<KingdomModel> Kingdom { get; set; }
+
+        Task OnInitializedAsync();
     }
+
     public class GenusViewModel : IGenusViewModel
     {
-        
+        private IDataAccess _data;
+        private IConfiguration _config;
+
+        public List<GenusModel> Genus { get; set; }
+        public List<KingdomModel> Kingdom { get; set; }
+
+        public GenusViewModel(IDataAccess data, IConfiguration config)
+        {
+            _data = data;
+            _config = config;
+        }
+
+        public async Task OnInitializedAsync()
+        {
+            string sql = "select * from genus";
+            string sql2 = "select * from kingdom";
+
+            Genus = await _data.LoadData<GenusModel, dynamic>(sql, new { }, _config.GetConnectionString("default"));
+            Kingdom = await _data.LoadData<KingdomModel, dynamic>(sql2, new { }, _config.GetConnectionString("default"));
+        }
     }
 }
